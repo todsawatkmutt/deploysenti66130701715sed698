8991e2c6da7e8b6bcb0bae6cb8d323db57eaa45a
todsawatkmutt <161877245+todsawatkmutt@users.noreply.github.com>
2024-11-08 09:43:54 +0700

    Add files via upload

---
+++ app_sa_nb66130701715.py
@@ -0,0 +1,22 @@
+import pickle
+import streamlit as st
+import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+# Load the model
+with open('66130701715sentiment_pipeline_model.pkl', 'rb') as model_file:
+    loaded_model = pickle.load(model_file)
+
+# Streamlit app
+st.title("Sentiment Analysis App")
+
+# Input text
+user_input = st.text_input("Enter a sentence:")
+
+if user_input:
+    # Make a prediction
+    prediction = loaded_model.predict([user_input])[0]
+    # Display the result
+    st.write("Sentiment:", "Positive" if prediction else "Negative")
